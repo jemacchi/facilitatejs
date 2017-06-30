d4a24721bf5388ab2294f771d3356a76a0f507c6
Jose <jose.macchi@gmail.com>
2017-06-30 16:51:37 -0300

    Voice feedback, region layout

--- js/app.js
+++ js/app.js
@@ -14,18 +14,49 @@ define([
 	facilitator = new Facilitator();
 	
     var init = function() {
+       facilitator.init();
+      
+        $(function() {
+            var _timeout,
+                _wait = 1000; // 2 seconds
+
+            $(document).mousemove(function() {
+                clearTimeout(_timeout);
+                _timeout = setTimeout(function() {
+                    //alert(facilitator.getTextualPosition());
+                    //Buscar el elemento en tal posicion
+                    //responsiveVoice.speak(facilitator.getText());
+
+    var element = $(':hover');
+    if(element.length)
+    {
+        var domElement = element[element.length - 1];
+        var tagName = domElement.tagName;
+        var id = domElement.id ? ' id="' + domElement.id + '"' : "";
+        responsiveVoice.speak(facilitator.getText(domElement));
+        //document.getElementById('test').innerHTML =
+        //"hover: &lt;" + tagName.toLowerCase() + id + "&gt;";
+    }
+
+                    //var posText = facilitator.getTextualPosition();
+                    //responsiveVoice.speak(posText);
+                }, _wait);
+            });
+        });
+
        document.querySelector( 'body' ).addEventListener( 'click', function( event ) {
           event.preventDefault();
           zoom.to({ element: event.target });
           responsiveVoice.speak(facilitator.getText(event.target));
 			 } );     
        $(document).on('keydown',null,'a', function () {
-         facilitator.showReference();
+         var pos = facilitator.showReference();
+         var posText = facilitator.getTextualPosition();
+         responsiveVoice.speak(posText);
        });
        $(document).on('keydown',null,'z', function () {
          facilitator.hideReference();
        });
-       
     };
 
     return {
--- js/helpers/facilitator.js
+++ js/helpers/facilitator.js
@@ -21,18 +21,52 @@ define([
     var Facilitator = Class.extend(
 	/** @lends Facilitator.prototype */
 	{
-	    init: function() {},
+      currentMousePos:null,
+	    init: function() {
+        $(function($) {
+            parent.currentMousePos = { x: -1, y: -1 };
+            $(document).mousemove(function(event) {
+                parent.currentMousePos.x = event.pageX;
+                parent.currentMousePos.y = event.pageY;
+            });
+        });
+      },
 	  /**
 		* getText from an element
 		*/
 		getText: function (elem) { 
       return $(elem).text(); 
     },
-    
+    getTextualPosition: function () { 
+      //var wSizeWidth = $(window).width();
+      //var wSizeHeight = $(window).height();
+      var w = window, 
+          d = document,
+          e = d.documentElement,
+          g = d.getElementsByTagName('body')[0],
+          x = w.innerWidth || e.clientWidth || g.clientWidth,
+          y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+      var pHorizontal = parent.currentMousePos.x/x ;
+      var pVertical = parent.currentMousePos.y/y ;
+      console.log(pHorizontal+'-'+pVertical);
+      var pReturn = '';
+      if (pHorizontal <= 0.25) pReturn += 'Left';
+      if (pHorizontal > 0.25 && pHorizontal <= 0.75) pReturn += 'Center';
+      if (pHorizontal > 0.75) pReturn += 'Right';
+      pReturn += ' ';
+      if (pVertical <= 0.25) pReturn += 'Top';
+      if (pVertical > 0.25 && pVertical <= 0.75) pReturn += 'Middle';
+      if (pVertical > 0.75) pReturn += 'Bottom';
+      return pReturn ;
+    },
     showReference: function () { 
-       $('body').plainOverlay('show');
-     },
-     
+       $('body').plainOverlay({
+          progress: function() { 
+            return ; 
+          }
+       }).plainOverlay('show');
+       return parent.currentMousePos;
+    },
     hideReference: function () { 
        $('body').plainOverlay('hide');
     },
